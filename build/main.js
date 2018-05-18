7675b3152f27ae6fb5debd05be2ed06e676feefe
KopylovVitalik <vitalik.kopylov@gmail.com>
2018-05-19 00:14:42 +0300

    desktop version

---
+++ build/main.js
@@ -0,0 +1,24 @@
+$(function() {
+  // Smooth Scrolling
+  $('a[href*="#"]:not([href="#testimonials-carousel"])').click(function() {
+    if (
+      location.pathname.replace(/^\//, '') ==
+        this.pathname.replace(/^\//, '') &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate(
+          {
+            scrollTop: target.offset().top
+          },
+          1500
+        );
+        return false;
+      }
+    }
+  });
+});
+
+$('.carousel').carousel();
